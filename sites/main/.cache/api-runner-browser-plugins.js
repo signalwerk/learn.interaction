6986211dcf08b07cc9b5c1a5f0f777f11c6ab64c
signalwerk <sh@signalwerk.ch>
2019-01-22 03:13:33 +0100

    MOD: change name

---
+++ sites/main/.cache/api-runner-browser-plugins.js
@@ -0,0 +1,10 @@
+module.exports = [{
+      plugin: require('/Users/glender/Desktop/CODE_GIT/learn.interaction/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/glender/Desktop/CODE_GIT/learn.interaction/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/glender/Desktop/CODE_GIT/learn.interaction/sites/main/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ sites/main/.cache/sync-requires.js
@@ -0,0 +1,17 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---themes-gatsby-theme-signalwerk-src-templates-post-index-js": hot(preferDefault(require("/Users/glender/Desktop/CODE_GIT/learn.interaction/themes/gatsby-theme-signalwerk/src/templates/Post/index.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/glender/Desktop/CODE_GIT/learn.interaction/sites/main/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/glender/Desktop/CODE_GIT/learn.interaction/sites/main/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/glender/Desktop/CODE_GIT/learn.interaction/sites/main/src/pages/index.js"))),
+  "component---src-pages-screens-abstract-md": hot(preferDefault(require("/Users/glender/Desktop/CODE_GIT/learn.interaction/sites/main/src/pages/screens/abstract.md"))),
+  "component---src-pages-root-md": hot(preferDefault(require("/Users/glender/Desktop/CODE_GIT/learn.interaction/sites/main/src/pages/root.md"))),
+  "component---src-pages-screens-examples-md": hot(preferDefault(require("/Users/glender/Desktop/CODE_GIT/learn.interaction/sites/main/src/pages/screens/examples.md"))),
+  "component---src-pages-screens-index-md": hot(preferDefault(require("/Users/glender/Desktop/CODE_GIT/learn.interaction/sites/main/src/pages/screens/index.md")))
+}
+
