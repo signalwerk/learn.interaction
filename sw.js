a32900def166bbc51af62a812134fb773129c27c
Travis CI <sh@signalwerk.ch>
2019-05-16 23:11:29 +0000

    Deploy to GitHub Pages: 95eb4b8669ad95956c119c5ce08404140d4d1e97

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9e14b70a047bd31e5c4f.js"
+    "url": "webpack-runtime-5ea238da8ae4b02bdf39.js"
   },
   {
     "url": "styles.b6035cbc59a35770d44c.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "05472d06e14c281938b0438653f9e497"
+    "revision": "da2957e72f90001f8edcd7b1ea7da62a"
   },
   {
     "url": "component---src-pages-404-js-dfed7ee4cecf8930ab64.js"
